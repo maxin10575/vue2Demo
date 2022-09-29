d011346434ef16303bf9a8f69ecf4e1b0a6c317e
maxin10575 <1057598919@qq.com>
2022-09-29 21:05:21 +0800

    1122

--- src/router/router.js
+++ src/router/router.js
@@ -12,5 +12,8 @@ export default [{
     }, {
         path: '/score',
         component: r => require.ensure([], () => r(require('../page/score')), 'score')
+    }, {
+        path: '/ibed',
+        component: r => require.ensure([], () => r(require('../page/ibed')), 'ibed')
     }]
 }]
